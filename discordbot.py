0cd871393b02c067a7b4b9873532654b3cea521c
pekopeko8585 <78493874+pekopeko8585@users.noreply.github.com>
2021-02-04 08:19:36 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,6 +5,8 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -17,5 +19,14 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pekodesu!!!')
 
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
 
 bot.run(token)
