c70a1dbe7ac86507814df96e4b009b29d7197d4e
pekopeko8585 <78493874+pekopeko8585@users.noreply.github.com>
2021-02-04 20:48:29 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -28,7 +28,7 @@ async def on_message(message):
     if message.author.bot:
         return
     
-    await message.channel.send('きてるよ')
+    await message.channel.send('きてるよ2')
         
     # システム日付を返す。
     if message.content == '/sysdate':
