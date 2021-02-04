4db70c0c5c56371ed875b7e56831225d950db674
pekopeko8585 <78493874+pekopeko8585@users.noreply.github.com>
2021-02-04 20:35:40 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -25,10 +25,10 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-@tasks.loop(seconds=60)
-async def loop():
-    channel = client.get_channel('806529550355791872') #発言チャンネルを指定
-    await channel.send('hoge')
+# @tasks.loop(seconds=60)
+# async def loop():
+    # channel = client.get_channel('806529550355791872') #発言チャンネルを指定
+    # await channel.send('hoge')
     
     # now = datetime.now().strftime('%H:%M')
     #12:00・18:00にニュースを自動取得する
