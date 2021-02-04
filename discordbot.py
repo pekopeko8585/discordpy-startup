289c5f6c6e3f751a0c4df2cd213cd632ef798553
pekopeko8585 <78493874+pekopeko8585@users.noreply.github.com>
2021-02-04 18:54:08 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -7,24 +7,26 @@ import datetime
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-dt_now = datetime.datetime.now()
-
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+client.run(token)
 
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
 
-@bot.command()
-async def peko(ctx, arg):
-    await ctx.send('引数は' + arg + 'です')
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
 
-@bot.command()
-async def システム日時(ctx):
-    await ctx.send('システム日付は' + dt_now.strftime('%Y-%m-%d %H:%M:%S') + 'です')
-
-bot.run(token)
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
