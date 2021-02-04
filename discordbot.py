80f34707344c2951651ae02c153f7d06fa08f27b
pekopeko8585 <78493874+pekopeko8585@users.noreply.github.com>
2021-02-04 12:10:30 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 import os
 import traceback
 import discord
+import datetime
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
@@ -20,14 +21,9 @@ async def on_command_error(ctx, error):
 async def peko(ctx, arg):
     await ctx.send('引数は' + arg + 'です')
 
-# メッセージ受信時に動作する処理
-@client.event
-async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
-        return
-    # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == '/neko':
-        await message.channel.send('にゃーん')
+@bot.command()
+async def システム日時(ctx):
+dt_now = datetime.datetime.now()
+    await ctx.send('システム日付は' + dt_now + 'です')
 
 bot.run(token)
