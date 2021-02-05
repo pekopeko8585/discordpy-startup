3813cfda25051fda90119afddfc5aaa558c48f14
pekopeko8585 <78493874+pekopeko8585@users.noreply.github.com>
2021-02-06 03:30:31 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -55,7 +55,7 @@ async def sendloop(channel):
     # 現在の時刻
     d_today = datetime.datetime.now()
     await channel.send(d_today.strftime('%Y-%m-%d %H:%M:%S'))
-    await channel.send(eventList[1] + ':' + d_today.strftime('%H:%M'))
+    await channel.send(eventList[0}[1] + ':' + d_today.strftime('%H:%M'))
     
     for item in eventList:
         if  d_today.weekday() == item[0] and item[1] == d_today.strftime('%H:%M'):
