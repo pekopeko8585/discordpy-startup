7ee37805dcd619b804001f196e0b04a19feae867
pekopeko8585 <78493874+pekopeko8585@users.noreply.github.com>
2021-02-24 06:16:22 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -207,6 +207,8 @@ async def sendloop(channel):
     for item in eventList_week:
         await channel.send(item[0])
         await channel.send(str(9))
+        await channel.send(item[0] == str(9))
+        
         # 曜日と日時が一致した場合
         if (item[0] == str(9) or item[0]) == str(get_nth_week(datetime.date.today().day)) and now_week == item[1] and item[2] == d_today.strftime('%H:%M'):            
             tempstr = tempstr + item[3] + '\n\n'
